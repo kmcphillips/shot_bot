178127d3b1ae275e71c067d6eb6239f0cf911d62
Kevin McPhillips <github@kevinmcphillips.ca>
2021-05-11 14:42:43 -0400

    Add polling for walmart location

--- lib/bots/walmart_bot.rb
+++ lib/bots/walmart_bot.rb
@@ -2,21 +2,22 @@
 class WalmartBot < BaseHTTPBot
   def notification
     {
-      title: "Found appointments at Walmart",
-      message: "TODO",
+      title: "Found appointment at Walmart South Keys",
+      message: "Walmart at 2210 Bank St. has an appointment. https://portal.healthmyself.net/walmarton/forms/Dpd",
     }
   end
 
   class Response < BaseHTTPBot::Response
     def initialize
-      @path = "http://" # TODO
-      @headers = {}
+      @path = "https://portal.healthmyself.net/walmarton/guest/booking/4752/schedules?locId=1180"
+      @headers = {
+        "Cookie" => "hm_session=#{ Global.config.walmart_bot.cookie }",
+      }
       @http_method = :get
     end
 
     def available?
-      # TODO
-      false
+      !!(result.body.index("next_date") || result.body.index("nextDate"))
     end
   end
 
