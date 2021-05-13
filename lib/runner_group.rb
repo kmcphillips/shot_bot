dc94b6ab5e80708f7cf36c6e63edfd2e3e90befa
Kevin McPhillips <github@kevinmcphillips.ca>
2021-05-13 19:15:44 -0400

    Guard config a bit

--- lib/runner_group.rb
+++ lib/runner_group.rb
@@ -6,12 +6,15 @@ class RunnerGroup
     @config = config
   end
 
-  def run # TODO this only runs one for now
+  def run
     Global.logger.info("Starting #{ config.count } #{ 'runner'.pluralize(config.count) }...")
 
     global_config = {
       interval_seconds: Global.config.interval_seconds, # TODO this could be moved to a different level
     }
+
+    raise "Cannot run zero Runners." if config.size == 0
+    raise "Cannot run more than one Runner for now. I have no idea if any of this is thread safe." if config.size > 1  # TODO this only runs one for now
 
     config.map do |runner_config|
       Runner.new(runner_config.to_h.merge(global_config)).run
