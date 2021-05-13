45fe948206f9114948fc347a34078996c252505b
Kevin McPhillips <github@kevinmcphillips.ca>
2021-05-13 12:29:42 -0400

    Move mail out of defaults into explicit SMTP config

--- lib/notifiers/email_notifier.rb
+++ lib/notifiers/email_notifier.rb
@@ -1,16 +1,4 @@
 # frozen_string_literal: true
-Mail.defaults do
-  delivery_method :smtp, {
-    address: Global.config.email.smtp_address,
-    port: Global.config.email.smtp_port,
-    domain: Global.config.email.smtp_domain,
-    user_name: Global.config.email.smtp_username,
-    password: Global.config.email.smtp_password,
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-end
-
 class EmailNotifier < NotifierBase
   def notify(title:, message:)
     Global.logger.info("[EmailNotifier] recipient=#{ recipient } title=#{ title } message=#{ message }")
@@ -22,12 +10,25 @@ class EmailNotifier < NotifierBase
       body: message,
       charset: 'UTF-8',
     )
+    mail.delivery_method(:smtp, smtp_options)
     mail.deliver
 
     mail
   end
 
   private
+
+  def smtp_options
+    {
+      address: Global.config.email.smtp_address,
+      port: Global.config.email.smtp_port,
+      domain: Global.config.email.smtp_domain,
+      user_name: Global.config.email.smtp_username,
+      password: Global.config.email.smtp_password,
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+  end
 
   def recipient
     Global.config.email.email_notifier_recipient
