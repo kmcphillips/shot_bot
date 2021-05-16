7f91e58c783eccce0d1ddc94926be16da0483867
Kevin McPhillips <github@kevinmcphillips.ca>
2021-05-16 08:26:50 -0400

    Allow locations to be filtered for shoppers

--- lib/bots/shoppers_bot.rb
+++ lib/bots/shoppers_bot.rb
@@ -8,23 +8,27 @@ class ShoppersBot < BaseBot
   end
 
   def build_response
-    ShoppersBot::Response.new(results_per_page: config[:results_per_page], latitude: config[:latitude], longitude: config[:longitude])
+    ShoppersBot::Response.new(results_per_page: config[:results_per_page], latitude: config[:latitude], longitude: config[:longitude], store_numbers: config[:store_numbers])
   end
 
   class Response < BaseHttpBotResponse
-    attr_reader :locations
+    attr_reader :locations, :store_numbers
 
-    def initialize(results_per_page:, latitude:, longitude:)
+    def initialize(results_per_page:, latitude:, longitude:, store_numbers: nil)
       super()
       results_per_page = 24
       @path = "https://www1.shoppersdrugmart.ca/en/store/getstores?latitude=#{ latitude }&longitude=#{ longitude }&radius=500&unit=km&lookup=nearby&filters=RSV-CVW:TRUE,RSV-COV:TRUE&rpp=#{ results_per_page }&isCovidShotSearch=true&getCovidShotAvailability=true"
-      puts @path
       @headers = {}
       @http_method = :get
-      @locations = []
+      @locations = nil
+      @store_numbers = store_numbers
+      @store_numbers = @store_numbers.map { |n| n.presence&.to_i }.compact if @store_numbers
     end
 
     def found?
+      return locations.any? if locations
+      @locations = []
+
       begin
         json = JSON.parse(result.body)
       rescue => e
@@ -33,7 +37,14 @@ class ShoppersBot < BaseBot
         return false
       end
 
-      json["results"].each do |json_result|
+      json_results = json["results"]
+
+      if store_numbers && store_numbers.any?
+        json_results = json_results.select { |json_result| store_numbers.include?(json_result['storeNumber'].to_i) }
+        Global.logger.info("[#{ self.class }] store_numbers=#{ store_numbers } limited to #{ json_results.count } #{ 'store'.pluralize(json_results.count) }")
+      end
+
+      json_results.each do |json_result|
         if json_result["FlusShotAvailableNow"]
           Global.logger.info("[#{ self.class }] FlusShotAvailableNow=true #{ json_result }")
           address = [ json_result['address'], json_result['address2'], json_result['address3'], json_result['city'] ].reject(&:blank?).join(", ")
